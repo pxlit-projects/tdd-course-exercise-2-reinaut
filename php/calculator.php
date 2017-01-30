4befa50ce511a33ac6f6815e48dec302bb846c07
Reinaut Krekels <reinaut.krekels@pxl.be>
2017-01-30 17:55:10 +0100

    extra tests en comments

--- php/calculator.php
+++ php/calculator.php
@@ -6,14 +6,20 @@ function calculate() {
 }
 
 function calculateFor($u) {
-	if (!preg_match('/^(?=[a-z]{2})(?=.{4,26})(?=[^.]*\.?[^.]*$)(?=[^_]*_?[^_]*$)[\w.]+$/iD',
+	// (?=[a-z]{2})			first, two a-z letters
+	// (?=.{3,26})			also, minimum 3, maximum 26 symbols (not lineends?)
+	// (?=[^.]*\.?[^.]*$)	also, zero or more no-dots, an optional dot, zero or more no-dots, end
+	// (?=[^_]*_?[^_]*$)	also, zero or more no-underscores, an optional underscore, zero or more no-underscores, end
+	// [\w.]+$ 				all-in-all, one or more \w-characters or dots, end
+	// /iD 					PCRE_CASELESS, PCRE_DOLLAR_ENDONLY ($ is real end)
+	if (!preg_match('/^(?=[a-z]{2})(?=.{3,26})(?=[^.]*\.?[^.]*$)(?=[^_]*_?[^_]*$)[\w.]+$/iD',
 	                $u))
 	{
-	 return false;
+		return false;
 	}
 	else
 	{
-	 return true;
+		return true;
 	}
 }
 
